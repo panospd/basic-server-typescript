e9de591a86c0a7f577509aa7c6740089bd1d5eac
panospd <anastapd@gmail.com>
2021-03-14 13:25:59 +0000

    Implemented login and logout

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hi there');
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['panospd'] }));
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
